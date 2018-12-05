ab5aad69707650140462d49e61fb3f16163820c7
YashVaidya <yashvsuperstar@gmail.com>
2018-12-04 23:31:54 -0500

    Update Map6.js

--- Map6.js
+++ Map6.js
@@ -1,10 +1,7 @@
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(
+    window.location.hostname === '[::1]' ||    window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
 );
@@ -20,10 +17,8 @@ export default function register() {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
-        // This is running on localhost. Lets check if a service worker still exists or not.
         checkValidServiceWorker(swUrl);
       } else {
-        // Is not local host. Just register service worker
         registerValidSW(swUrl);
       }
     });
